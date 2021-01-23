d08db09d98b46eccd6894a67883205a081e8e524
Ryan <ryaneckere1@gmail.com>
2021-01-23 04:57:45 -0500

    Built user model

---
+++ models/user.js
@@ -0,0 +1,17 @@
+// username store string   null = false
+// passwordhash store string  null = false
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('user', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    passwordhash: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return User;
+};
